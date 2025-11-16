edd6ed8fa85524b2d0d1f6822e55b45d63da4850
Socee-tech <sospeterngeresa@gmail.com>
2025-11-16 19:12:19 +0300

    first upload 1.2

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_REMOTE_URI);
+    await mongoose.connect(MONGO_REMOTE_URI);
     console.log("MongoDb connected...");
   } catch (error) {
     console.log(error.message);
