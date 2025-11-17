2b698adb7362d579959386fbf7d6174a4ea5fc20
Socee-tech <sospeterngeresa@gmail.com>
2025-11-17 10:40:56 +0300

    initial commit

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,6 @@ import connectDb from "./config/db.js";
 import studentController from "./controllers/studentController.js";
 import courseController from "./controllers/courseController.js";
 import lecturerController from "./controllers/lecturerController.js";
-import serverless from "serverless-http";
 
 dotenv.config();
 connectDb();
@@ -23,11 +22,5 @@ App.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-// Start a local server for development (nodemon / local testing)
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  App.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
-
-// Export a serverless handler for Vercel / other serverless platforms
-export default serverless(App);
+const PORT = process.env.PORT || 5000;
+App.listen(PORT, () => console.log(`Server running on port ${PORT}`));
